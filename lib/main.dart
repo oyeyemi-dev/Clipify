e1d74965f05a488dd6a463d9cddb94e553685cae
oyeyemi-dev <oyelol489@gmail.com>
2025-08-16 14:45:06 +0100

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ClipifyApp());
+}
+
+class ClipifyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Clipify',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Clipify - Simple Video Editor"),
+      ),
+      body: Center(
+        child: Text(
+          "Welcome to Clipify 🎬\nYour lightweight video editing app!",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
